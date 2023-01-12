59ac065ff4dea075c051e0a0c72fb5fe21c8ff38
alondramora <mora_ac@yahoo.com>
2023-01-11 21:36:12 -0800

    got the home route file set up and server file set up

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-// environment variables
+// Env variables
 const express = require('express');
 const app = express();
 const PORT = 8001;
 const mongoose = require('mongoose');
+
+const connectDB = require('./config/database.js');
+const homeRoutes = require('./routes/home');
+require('dotenv').config({ path: './config/.env' });
+
+//Connect to database
+connectDB();
+
+// Middleware
+app.set('view engine', 'ejs');
+app.set(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+// Routes
+app.use('/', homeRoutes);
+
+
+
+app.listen(PORT, () => console.log(`Server is running YO! Port: ${PORT}`));
