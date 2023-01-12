e78fdb7c6056a8a07687efa3c1042a028e0a17e2
alondramora <mora_ac@yahoo.com>
2023-01-11 21:15:45 -0800

    added more to users schema

--- models/users.js
+++ models/users.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-
+    firstName = {
+        type: String,
+        required: true,
+    },
+    lastName = {
+        type: String,
+        required: true,
+    },
+    userName = {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email = {
+        type: String,
+        required: true,
+        unique: true,
+    },
 });
 
-module.exports = mongoose.model('userList', userSchema, 'users');
+module.exports = mongoose.model('User', userSchema, 'people');
