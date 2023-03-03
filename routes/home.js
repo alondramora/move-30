4b47cfa8c7636a88d720a466718280698f3aceee
alondramora <mora_ac@yahoo.com>
2023-03-02 22:00:50 -0800

    changed up styling for homepage

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const homeController = require('../controllers/home');
-const upload = require('../middleware/multer');
+const homeController = require("../controllers/home");
+const upload = require("../middleware/multer");
+const passportConfig = require("../config/passport");
 
-router.get('/', homeController.getIndex);
-router.get('/about', homeController.getAbout);
-router.post('/new', upload.single('file'), homeController.createPost);
+router.get("/", homeController.getIndex);
+router.get("/about", homeController.getAbout);
+router.post("/new", upload.single("file"), homeController.createPost);
 // upload is a multer method that will store our file until we're ready to write it to cloudinary
-router.get('/profile', homeController.getProfile);
+router.get("/profile", homeController.getProfile);
+// router.get(
+//   "/profile",
+//   passportConfig.isAuthenticated,
+//   homeController.getProfile
+// );
 
 module.exports = router;
